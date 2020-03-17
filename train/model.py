75278c39f3869a5c1db82fecbf8bcb99a1959ca9
edumunozsala <edumunozsala@gmail.com>
2020-03-17 18:15:55 +0000

    update model to GRU

--- train/model.py
+++ train/model.py
@@ -14,10 +14,11 @@ class LSTMClassifier(nn.Module):
 
         self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
         ### UPDATED ###
-        # Added a n_layers and dropout parameters
+        # Use a GRU unit and added a n_layers and dropout parameters
         # We should probably include these parameters as parameters but for simplicity
         # we devide to "hardcode" the values, instead of not being a best practice
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=2, bidirectional=True)
+        self.lstm = nn.GRU(embedding_dim, hidden_dim, num_layers=2, bidirectional=True)
+        # Because of the bidirectional option we need to double the input dim
         self.dense = nn.Linear(in_features=hidden_dim*2, out_features=1)
         # Added a dropout layer
         self.dropout = nn.Dropout(0.3)
@@ -35,17 +36,13 @@ class LSTMClassifier(nn.Module):
         reviews = x[1:,:]
         ### UPDATED ###
         embeds = self.dropout(self.embedding(reviews))
-        #lstm_out, _ = self.lstm(embeds)
-        #out = self.dense(lstm_out)
         ## UPDATED ##
-        lstm_out, (hidden, cell) = self.lstm(embeds)
+        lstm_out, hidden = self.lstm(embeds)
         #concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
         #and apply dropout
         hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
         out = self.dense(hidden)
         
         ### UPDATED ###
-        # Dropout
-        #dout = self.dropout(out)
         #out = dout[lengths - 1, range(len(lengths))]
         return self.sig(out.squeeze())
